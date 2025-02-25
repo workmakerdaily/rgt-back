d476016b4b7bf834cc31d541ec952522b37412f2
workmakerdaily <sver12@naver.com>
2025-02-25 17:37:26 +0900

    25.02.25 17:#7

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,20 @@
-import express, { Request, Response } from 'express';
-import { Pool } from 'pg';
+import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes';
+import bookRoutes from './routes/bookRoutes';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// PostgreSQL 연결
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }, // Render 보안 연결
-});
+app.use(cors());
+app.use(express.json());
 
-// 기본 라우터
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Render + TypeScript + PostgreSQL!');
-});
-
-// DB 연결 테스트
-app.get('/db', async (req: Request, res: Response) => {
-    try {
-        const result = await pool.query('SELECT NOW()');
-        res.json(result.rows);
-    } catch (err: any) {
-        console.error('DB Error:', err.message);
-        console.error('Full Error:', err); 
-        res.status(500).send('DB Error');
-    }
-});
-
+// 라우터 등록
+app.use('/api/auth', authRoutes);  // 로그인, 회원가입
+app.use('/api/books', bookRoutes); // 책 관련 API
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
